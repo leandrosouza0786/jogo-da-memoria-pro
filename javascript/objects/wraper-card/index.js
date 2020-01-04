6ce6925aa778523c70c18bc165a7cdc69e28b667
unknown <leandrosouza0786@gmail.com>
2020-01-04 10:46:53 -0300

    feat(memory-game): adiciona arquitetura de componentes

--- javascript/objects/wraper-card/index.js
+++
@@ -1,6 +0,0 @@
-const $root = document.querySelector('#root');
-const $wrapCard = document.createElement('section');
-
-$wrapCard.classList.add('wrap-cards');
-
-$root.insertBefore($wrapCard, null);
--- javascript/pages/game/index.js
+++ javascript/pages/game/index.js
@@ -1,11 +1,50 @@
+const $root = document.querySelector("#root");
+const $cardsWrapper = createCardsWrapper();
 
-createCardMemory();
-createCardMemory();
-createCardMemory();
-createCardMemory();
-createCardMemory();
-createCardMemory();
+const createCardMemory = memoryCard();
+console.log("teste retorno function", createCardMemory)
+const $GueioBack = createCardMemory({
+    nameClass: '',
+    src: "assets/icon-collabcode.png",
+    alt: "Icone de um livro de C#"
+});
+
+const $MemoryCardBackCharp = createCardMemory({
+    nameClass: '-front',
+    src: "assets/icon-c.png",
+    alt: "Icone de um livro de C#"
+});
+
+const $MemoryCardBackJs = createCardMemory({
+    nameClass: '-front',
+    src: "assets/-icon-js.jpg",
+    alt: "Icone de um livro de C#"
+});
 
 
-createCardMemoryFront();
-createCardMemoryFront();
+const $MemoryCardBackJava = createCardMemory({
+    nameClass: '-front',
+    src: "assets/icon-java.jpg",
+    alt: "Icone de um livro de C#"
+});
+
+const $MemoryCardBackWoman = createCardMemory({
+    nameClass: '-front',
+    src: "assets/icon-woman.jpg",
+    alt: "Icone de um livro de C#"
+});
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $GueioBack);
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackWoman);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackWoman);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackJs);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackJs);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackJava);
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackCharp);
+$cardsWrapper.insertAdjacentHTML("beforeend", $MemoryCardBackCharp);
+
+
+$root.insertAdjacentElement("beforeend", $cardsWrapper);
